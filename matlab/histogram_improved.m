e9c362354e4aa1bd0694b189d1335c118cd6e15d
Jmq14 <jmq14@mails.tsinghua.edu.cn>
2016-05-17 01:27:45 +0800

    Improved colour histogram
    由于颜色直方图有个很大的问题在于，相似的颜色可能距离计算出来特别远，一方面这个可以改造对相似度计算函数，把简单的欧式距离变成所有维度分别加权计算
    后的距离，同时这也可以在特征提取的时候实现。比如我可以把属于某个bin的颜色也分配一部分到相邻的bin中。这里是一个基于此想法的简单实现。

---
+++ matlab/histogram_improved.m
@@ -0,0 +1,62 @@
+%Load Image
+clear
+path = './image/';
+img_path_list = dir(strcat(path,'*.JPEG')); 
+img_num = length(img_path_list);
+
+output = fopen('histogram.txt','wt');
+
+grpnum_R = 2;
+grpnum_G = 2;
+grpnum_B = 2;
+range_r = 256 / grpnum_R;
+range_g = 256 / grpnum_G;
+range_b = 256 / grpnum_B;
+if (mod(256, grpnum_R) > 0) 
+    grpnum_R = grpnum_R+1;
+end
+if (mod(256, grpnum_G) > 0) 
+    grpnum_G = grpnum_G+1;
+end;
+if (mod(256, grpnum_B) > 0) 
+    grpnum_B = grpnum_B+1;
+end
+
+feature = zeros(grpnum_R*grpnum_G*grpnum_B, 1);
+
+if img_num > 0
+    fprintf(output, '%d\n', img_num);
+    for k = 1 : img_num
+        fprintf('%dth image\n', k);
+        name = img_path_list(k).name;
+        img = imread(strcat(path, name));
+        
+        [m, n, c] = size(img);
+        for i = 1:m
+            for j = 1:n 
+               
+                r = fix(double(img(i,j,1))/range_r);
+                g = fix(double(img(i,j,2))/range_g);
+                b = fix(double(img(i,j,3))/range_b);
+                p = r*grpnum_G*grpnum_B + g*grpnum_B + b + 1;
+                feature(p, 1) = feature(p, 1)+1;
+                if p > 1
+                    feature(p-1, 1) = feature(p-1,1)+0.5;
+                end
+                if p < length(feature)
+                    feature(p+1, 1) = feature(p+1,1)+0.5;
+                end
+            end
+        end
+        a = sum(feature(:,1));
+        for i = 1:length(feature)
+            feature(i) = feature(i)/a;
+        end
+        fprintf(output, '%s', name);
+        fprintf(output, ' %f', feature);
+        fprintf(output, '\n');
+    end
+end
+
+fclose(output);
+
